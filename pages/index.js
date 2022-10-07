0be0320313147baeef0a88658c279e3462a7b9b7
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-07 16:23:46 -0300

    refactor: avoi to change initial response from API

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
 export async function getStaticProps() {
-  const maxPokemons = 251;
+  const maxPokemons = 150;
   const api = `https://pokeapi.co/api/v2/pokemon/`;
+  const response = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await response.json();
+  const { results } = data;
 
-  const res = await fetch(`${api}/?limit=${maxPokemons}`);
-
-  const data = await res.json();
-
-  // add pokemon index
-  data.results.forEach((item, index) => {
-    item.id = index + 1;
-  });
+  // add pokemon id
+  const pokemons = results.map((pokemon, index) => ({
+    ...pokemon,
+    id: index + 1,
+  }));
 
   return {
     props: {
-      pokemons: data.results,
+      pokemons,
     },
   };
 }
@@ -24,7 +24,9 @@ export default function Home({ pokemons }) {
       <h1>Pokedex</h1>
       <ul>
         {pokemons.map((pokemon) => (
-          <li key={pokemon.id}>{pokemon.name}</li>
+          <li key={pokemon.id}>
+            {pokemon.id} - {pokemon.name}
+          </li>
         ))}
       </ul>
     </div>
