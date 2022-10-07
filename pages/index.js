83ac60cd8e23050311c19e46afaa57021f2e82b6
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-07 14:57:28 -0300

    feat: fetch and show pokemons names on home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
-export default function Home() {
+export async function getStaticProps() {
+  const maxPokemons = 251;
+  const api = `https://pokeapi.co/api/v2/pokemon/`;
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+
+  const data = await res.json();
+
+  // add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }) {
   return (
     <div className="container">
       <h1>Pokedex</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
