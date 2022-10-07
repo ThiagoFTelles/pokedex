14d9f4eeb43eadb444621ad09913909ec2bfc146
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-07 17:51:12 -0300

    feat: add pokemon image

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,20 @@
+import Image from 'next/image';
 import React from 'react';
 
 export default function Card({ pokemon }) {
+  const { id, name } = pokemon;
+
   return (
     <div>
-      <h3>{pokemon.name}</h3>
+      <Image
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`}
+        width="120"
+        height="120"
+        alt={name}
+      />
+      <h3>
+        {id} - {name}
+      </h3>
     </div>
   );
 }
