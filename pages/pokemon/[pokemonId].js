d7dbb5fb7003f51ff53730487a12898588e78fbe
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-08 15:31:10 -0300

    refactor: use Sass files

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,4 +1,4 @@
-import styles from '../../styles/Pokemon.module.css';
+import styles from '../../styles/Pokemon.module.scss';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
