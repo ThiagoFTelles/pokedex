54be10b1532d3fc89efdf8411a1bf3189f911e32
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-08 16:13:21 -0300

    feat: show only 3 pokemons in home

--- components/Card.js
+++ components/Card.js
@@ -1,20 +1,20 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import styles from '../styles/Card.module.scss';
 
 export default function Card({ pokemon }) {
   const { id, name } = pokemon;
 
   return (
-    <div>
+    <div className={styles.card}>
       <Image
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`}
         width="70"
         height="70"
         alt={name}
       />
-      <p>#{id}</p>
-      <h3>{name}</h3>
+      <h3>{`#${id} ${name}`}</h3>
       <Link href={`/pokemon/${id}`}>
         <a>Details</a>
       </Link>
