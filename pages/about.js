3d5b23ba9d8e07a9ab0bad8ac122b1e3e75a6740
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-10 00:09:20 -0300

    chore: change style

--- pages/about.js
+++ pages/about.js
@@ -4,18 +4,41 @@ import styles from '../styles/About.module.scss';
 export default function About() {
   return (
     <div className={`container ${styles.about}`}>
-      <h1>About</h1>
-      <p>
-        Project created using React + Nextjs + Sass + Redux for training
-        purposes with these tools. I created a design that is fun and allows the
-        use of componentization, navigation between routes and API consumption.
-      </p>
-      <Image
-        src="/images/charizard.png"
-        width="300"
-        height="300"
-        alt="Charizard"
-      />
+      <span className={styles.title}>
+        <Image
+          src="/images/pokeball.gif"
+          width="50"
+          height="50"
+          alt="Charizard"
+        />
+        <h1>About</h1>
+        <Image
+          src="/images/pokeball.gif"
+          width="50"
+          height="50"
+          alt="Charizard"
+        />
+      </span>
+      <span className={styles.text}>
+        <p>
+          Go to the homepage, click or tap to catch a pokemon from the list to
+          hear and see its information on the Pokedex.
+        </p>
+        <p>
+          Project created using React + Nextjs + Sass + Redux for training
+          purposes with these tools. I created a design that is fun and allows
+          the use of componentization, navigation between routes and API
+          consumption.
+        </p>
+      </span>
+      <span className={styles.image}>
+        <Image
+          src="/images/charizard.png"
+          width="250"
+          height="250"
+          alt="Charizard"
+        />
+      </span>
     </div>
   );
 }
